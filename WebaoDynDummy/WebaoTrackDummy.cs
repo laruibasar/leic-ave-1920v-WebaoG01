3585334b67a4752f77a812ca5c98a3add3ae5c79
António Manuel Bacao Pereira <ambccp@gmail.com>
2020-04-19 16:01:44 +0100

    WebaoDynTrack
    WebaoDynTrack done.
    WebaoTrackDummy.cs created
    TODO:Implement WebaoTrackDummy

---
+++ WebaoDynDummy/WebaoTrackDummy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Webao;
+using Webao.Attributes;
+using WebaoDynamic;
+using WebaoTestProject.Dto;
+
+namespace WebaoDynDummy
+{
+    class WebaoTrackDummy
+    {
+    }
+}

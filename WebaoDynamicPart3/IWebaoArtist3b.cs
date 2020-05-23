7da3d273e9f6ae7320d1d7721c0c057abfcb96d3
gordett <gordett@gmail.com>
2020-05-23 15:59:36 +0100

    com erros

---
+++ WebaoDynamicPart3/IWebaoArtist3b.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WebaoTestProject.Dto;
+
+namespace WebaoDynamic
+{
+    public interface IWebaoArtist3b
+    {
+        Artist GetInfo(string name);
+        List<Artist> Search(string name, int page);
+    }
+}
---
+++ WebaoTestProject/WebaoDynamicTest3b.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Webao;
+using WebaoDynamic;
+using WebaoDynDummy;
+using WebaoTestProject.Dto;
+
+namespace WebaoTestProject
+{
+    [TestFixture]
+    public class WebaoDynamicTest3b 
+    {
+        
+
+    }
+}

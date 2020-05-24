242fcfd1e16d6c6134864053c53b589f13ea3d8e
Luis Bandarra <luis.bandarra@homestudio.pt>
2020-05-24 10:46:34 +0100

    Feature (Work 3-1): Copy dynamics for adaptation

---
+++ WebaoDynamicsDelegates/WebaoDyn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Webao;
+
+namespace WebaoDynamicsDelegates
+{
+    public class WebaoDyn
+    {
+        private readonly IRequest req;
+        protected readonly Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+        public WebaoDyn(IRequest req)
+        {
+            this.req = req;
+        }
+
+        public void SetUrl(string url)
+        {
+            req.BaseUrl(url);
+        }
+
+        public void SetParameter(string key, string value)
+        {
+            req.AddParameter(key, value);
+        }
+
+        public object GetRequest(string path, Type requestType)
+        {
+            return req.Get(path, requestType);
+        }
+    }
+}
